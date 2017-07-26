d47101ee2ec83d661fb189c8a90e122b09464fbf
jgittler <jason.gittler@gmail.com>
2017-07-26 13:08:01 -0700

    auth

--- checkouts.rb
+++ checkouts.rb
@@ -2,11 +2,15 @@ require "sinatra"
 require "json"
 
 get "/" do
-  @checkouts = Dir.glob("*.json").map do |file|
-    JSON.parse(File.read(file))
-  end
+  if params["secret"] == secret
+    @checkouts = Dir.glob("*.json").map do |file|
+      JSON.parse(File.read(file))
+    end
 
-  erb :index
+    erb :index
+  else
+    status 401
+  end
 end
 
 post "/create" do
@@ -27,4 +31,8 @@ def build_blob(params)
     item: params["item"],
     time: Time.now
   }.to_json
+end
+
+def secret
+  File.read(".secret")
 end
