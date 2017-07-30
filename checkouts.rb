8bce801f80aae989e6a2b750d1fd9d13bd334737
jgittler <jason.gittler@gmail.com>
2017-07-30 09:34:00 -0700

    configure email

--- checkouts.rb
+++ checkouts.rb
@@ -3,7 +3,7 @@ require "json"
 require "pony"
 
 get "/" do
-  if params["secret"] == _secret
+  if params["secret"] == ENV["SECRET"]
     @checkouts = Dir.glob("*.json").map do |file|
       JSON.parse(File.read(file))
     end
@@ -22,15 +22,29 @@ post "/create" do
     f.write(data)
   end
 
-  Pony.mail(
-    to: "jason@felloeyewear.com",
-    cc: "jonathan@felloeyewear.com",
-    from: "checkouts@felloeyewear.com",
-    subject: "New Checkout Started",
-    body: data.inspect
-  )
+  _send_email(data)
 
   status 201
+end
+
+def _send_email(data)
+  Pony.mail({
+    to: "jason@felloeyewear.com",
+    cc: "jonathan@felloeyewear.com",
+    from: "contact@felloeyewear.com",
+    subject: "New Checkout Started",
+    body: data.inspect
+    via: :smtp,
+    via_options: {
+      :address              => "smtp.gmail.com",
+      :port                 => "587",
+      :enable_starttls_auto => true,
+      :user_name            => "contact@felloeyewear",
+      :password             => ENV["EMAIL_PASSWORD"],
+      :authentication       => :plain,
+      :domain               => "gmail.com"
+    }
+  })
 end
 
 def _build_blob(params)
@@ -40,8 +54,4 @@ def _build_blob(params)
     item: params["item"],
     time: Time.now
   }.to_json
-end
-
-def _secret
-  "somecrazysecretpeoplewonthave"
 end
