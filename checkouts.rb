265488d145b8559361d1d3793cb52a000d742cdd
jgittler <jason.gittler@gmail.com>
2017-07-30 09:17:06 -0700

    email the checkouts

--- checkouts.rb
+++ checkouts.rb
@@ -1,8 +1,9 @@
 require "sinatra"
 require "json"
+require "pony"
 
 get "/" do
-  if params["secret"] == secret
+  if params["secret"] == _secret
     @checkouts = Dir.glob("*.json").map do |file|
       JSON.parse(File.read(file))
     end
@@ -15,16 +16,24 @@ end
 
 post "/create" do
   email = params["email"]
+  data = _build_blob(params)
 
-  File.open("#{email}-#{Time.now}.json", "w+") do |f|
-    f.write(build_blob(params))
+  File.open("./#{email}-#{Time.now}.json", "w+") do |f|
+    f.write(data)
   end
+
+  Pony.mail(
+    to: "jason@felloeyewear.com",
+    cc: "jonathan@felloeyewear.com",
+    from: "checkouts@felloeyewear.com",
+    subject: "New Checkout Started",
+    body: data.inspect
+  )
 
   status 201
 end
 
-
-def build_blob(params)
+def _build_blob(params)
   {
     email: params["email"],
     device: params["device"],
@@ -33,6 +42,6 @@ def build_blob(params)
   }.to_json
 end
 
-def secret
+def _secret
   "somecrazysecretpeoplewonthave"
 end
